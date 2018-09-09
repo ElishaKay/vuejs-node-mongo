a37361389739e46faf8f76772a26ca28a0dfcc2b
Elisha Kramer <kramer1346@gmail.com>
2018-09-10 00:39:37 +0200

    tenants info

--- client/src/components/Edit.vue
+++ client/src/components/Edit.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="edit container">
     <Alert v-if="alert" v-bind:message="alert" />
-    <h1 class="page-header">Edit Customer</h1>
-    <form v-on:submit="updateCustomer">
+    <h1 class="page-header">Edit Tenant</h1>
+    <form v-on:submit="updateTenant">
         <div class="well">
-            <h4>Customer Info</h4>
+            <h4>Tenant Info</h4>
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" class="form-control" placeholder="Name" v-model="customer.name">
+                <input type="text" class="form-control" placeholder="Name" v-model="tenant.name">
             </div>
         </div>
         <div class="well">
-            <h4>Customer Contact</h4>
+            <h4>Tenant Contact</h4>
             <div class="form-group">
                 <label>Email</label>
-                <input type="text" class="form-control" placeholder="Email" v-model="customer.email">
+                <input type="text" class="form-control" placeholder="Email" v-model="tenant.email">
             </div>
             <div class="form-group">
                 <label>Phone</label>
-                <input type="text" class="form-control" placeholder="Phone" v-model="customer.phone">
+                <input type="text" class="form-control" placeholder="Phone" v-model="tenant.phone">
             </div>
         </div>
 
         <div class="well">
-            <h4>Customer Location</h4>
+            <h4>Tenant Location</h4>
             <div class="form-group">
                 <label>Address</label>
-                <input type="text" class="form-control" placeholder="Address" v-model="customer.address">
+                <input type="text" class="form-control" placeholder="Address" v-model="tenant.address">
             </div>
         </div>
          <div class="well">
-            <h4>Customer Financials</h4>
+            <h4>Tenant Financials</h4>
             <div class="form-group">
                 <label>Debt</label>
-                <input type="number" class="form-control" placeholder="Debt" v-model.number="customer.debt">
+                <input type="number" class="form-control" placeholder="Debt" v-model.number="tenant.debt">
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
@@ -47,32 +47,32 @@
     name: 'add',
     data () {
         return {
-        customer: {},
+        tenant: {},
         alert:''
         }
     },
     methods: {
-        fetchCustomer(id){
+        fetchTenant(id){
             this.$http.get('http://localhost:3000/api/tenants/'+id)
             .then(function(response){
-                this.customer = response.body.tenant;
+                this.tenant = response.body.tenant;
             });
         },
-        updateCustomer(e){
-            if(!this.customer.name || !this.customer.email){
+        updateTenant(e){
+            if(!this.tenant.name || !this.tenant.email){
                 this.alert = 'Please fill in all required fields';
             } else {
-                let updCustomer = {
-                    name: this.customer.name,
-                    phone: this.customer.phone,
-                    email: this.customer.email,
-                    address: this.customer.address,
-                    debt: this.customer.debt
+                let updTenant = {
+                    name: this.tenant.name,
+                    phone: this.tenant.phone,
+                    email: this.tenant.email,
+                    address: this.tenant.address,
+                    debt: this.tenant.debt
                 }
 
-                this.$http.put('http://localhost:3000/api/tenants/'+this.$route.params.id, updCustomer)
+                this.$http.put('http://localhost:3000/api/tenants/'+this.$route.params.id, updTenant)
                     .then(function(response){
-                        this.$router.push({path: '/', query: {alert: 'Customer Updated'}});
+                        this.$router.push({path: '/', query: {alert: 'Tenant Updated'}});
                     });
 
                 e.preventDefault();
@@ -81,7 +81,7 @@
         }
     },
     created: function(){
-        this.fetchCustomer(this.$route.params.id);
+        this.fetchTenant(this.$route.params.id);
     },
     components:{
         Alert
--- client/src/components/Tenants.vue
+++ client/src/components/Tenants.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="customers container">
+  <div class="tenants container">
     <Alert v-if="alert" v-bind:message="alert" />
-    <h1 class="page-header">Manage Customers</h1>
+    <h1 class="page-header">Manage Tenants</h1>
     <input class="form-control" placeholder="Enter Name" v-model="filterInput">
     <br />
     <table class="table table-striped">
@@ -16,12 +16,12 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="customer in filterBy(customers, filterInput)">
-            <td>{{customer.name}}</td>
-            <td>{{customer.address}}</td>
-            <td>{{customer.email}}</td>
-            <td>{{customer.debt}}</td>
-            <td><router-link class="btn btn-default" v-bind:to="'/customer/'+customer.id">View</router-link></td>
+          <tr v-for="tenant in filterBy(tenants, filterInput)">
+            <td>{{tenant.name}}</td>
+            <td>{{tenant.address}}</td>
+            <td>{{tenant.email}}</td>
+            <td>{{tenant.debt}}</td>
+            <td><router-link class="btn btn-default" v-bind:to="'/tenant/'+tenant.id">View</router-link></td>
           </tr>
         </tbody>
     </table>
@@ -31,29 +31,29 @@
 <script>
   import Alert from './Alert';
   export default {
-    name: 'customers',
+    name: 'tenants',
     data () {
       return {
-        customers: [],
+        tenants: [],
         alert:'',
         filterInput:''
       }
     },
     methods: {
-      fetchCustomers(){
+      fetchTenants(){
         this.$http.get('http://localhost:3000/api/tenants/')
           .then(function(response){
-            this.customers = response.data.tenants;
+            this.tenants = response.data.tenants;
           });
       },
       filterBy(list, value){
-        return list.filter(function(customer){
-          return customer.name.indexOf(value) > -1;
+        return list.filter(function(tenant){
+          return tenant.name.indexOf(value) > -1;
         });
       }
     },
     created: function(){
-      this.fetchCustomers();
+      this.fetchTenants();
     }
   }
 </script>
